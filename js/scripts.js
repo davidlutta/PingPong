d467f5022ef24cd89b8eab813c3545ac848c9ad7
davidlutta <davidlutta2010@gmail.com>
2018-03-29 11:25:17 +0300

    Added inde.Html page

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+//
+// //Back-End :
+// var three  = function() {
+// var user3 = parseInt(prompt("Give me another !"));
+//   if (user3 % 3 ===0){
+//   alert("ping");
+//   } else {
+//   alert("false")
+//   }
+// }
+// three();
+//
+// var five  = function() {
+// var user2 = parseInt(prompt("Give me another !"));
+//   if (user2 % 5 ===0){
+//   alert("pong");
+//   } else {
+//   alert("false")
+//   }
+// }
+// five();
+//
+// var fifteen = function() {
+// var user = parseInt(prompt("give me a number !"));
+//   if ((user % 3 === 0) && (user % 5 === 0) ){
+//     alert("pingpong");
+//     } else {
+//     alert("false");
+//     }
+// }
+// fifteen();
+//
+// //Front-End:
