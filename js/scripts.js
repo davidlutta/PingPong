14dbb698a55576c9c57028bc2581f9d159832b04
davidlutta <davidlutta2010@gmail.com>
2018-03-29 16:54:26 +0300

    :Added Front-End

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,22 @@
-//
-// //Back-End :
-.function(number){
-  var numberPingPong = [];
-  for(var index = 0; index <= number; index += 1){
-    if (index % 15 === 0) {
-      numberPingPong.push("PingPong");
-    } else if ( index % 5 === 0) {
-      numberPingPong.push("Pong");
-    } else if(index %3 === 0 ) {
-      numberPingPong.push("ping");
-    } else {
-      return number;
-    }
+function pingpong(startPoint) {
+  if ((startPoint % 3 === 0 && startPoint % 5 === 0)) {
+    return ('pingpong');
+  } else if (startPoint % 5 === 0) {
+    return ('pong');
+  } else if (startPoint % 3 === 0) {
+    return ('ping');
+  } else {
+    return startPoint;
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-// var three  = function() {
-// var user3 = parseInt(prompt("Give me another !"));
-//   if (user3 % 3 ===0){
-//   alert("ping");
-//   } else {
-//   alert("false")
-//   }
-// }
-// three();
-//
-// var five  = function() {
-// var user2 = parseInt(prompt("Give me another !"));
-//   if (user2 % 5 ===0){
-//   alert("pong");
-//   } else {
-//   alert("false")
-//   }
-// }
-// five();
-//
-// var fifteen = function() {
-// var user = parseInt(prompt("give me a number !"));
-//   if ((user % 3 === 0) && (user % 5 === 0) ){
-//     alert("pingpong");
-//     } else {
-//     alert("false");
-//     }
-// }
-// fifteen();
-//
-// //Front-End:
+$(document).ready(function() {
+  $('#b').click(function(event) {
+    var userNumber = parseInt($('input#pan').val());
+    event.preventDefault();
+    
+    for (var startPoint = 1; startPoint <= userNumber; startPoint += 1) {
+      $('ul.pet').append('<li>' + pingpong(startPoint) + '</li>');
+    }
+  });
+});
